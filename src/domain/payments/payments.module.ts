9c716caf25e63ebaa1f6db46ce04fe045438edb7
Han Myat Thu <hanmyatthu111@gmail.com>
2024-06-11 14:13:20 +0700

    implement payment logic

--- src/domain/payments/payments.module.ts
+++ src/domain/payments/payments.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { PaymentsService } from './payments.service';
 import { PaymentsController } from './payments.controller';
 import { Payment } from './entities/payment.entity';
+import { Order } from 'order/entities/order.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Payment])],
+  imports: [TypeOrmModule.forFeature([Payment, Order])],
   controllers: [PaymentsController],
   providers: [PaymentsService],
 })
--- src/domain/payments/payments.service.ts
+++ src/domain/payments/payments.service.ts
@@ -1,4 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Payment } from './entities/payment.entity';
+import { Repository } from 'typeorm';
+import { Order } from 'order/entities/order.entity';
+import { OrderStatus } from 'order/enums/order-status.enum';
 
 @Injectable()
-export class PaymentsService {}
+export class PaymentsService {
+
+  constructor(
+    @InjectRepository(Payment)
+    private readonly paymentRepository: Repository<Payment>,
+
+    @InjectRepository(Order)
+    private readonly orderRepository: Repository<Order>,
+  ) {}
+
+  async payOrder(id: number) {
+    const order = await this.orderRepository.findOne({
+      where: { id },
+      relations: {
+        payment: true
+      }
+    })
+
+    if (!order) {
+      throw new NotFoundException('Order Not Found');
+    }
+
+    if (order.payment) {
+      throw new ConflictException('Order is already paid');
+    }
+
+    const payment = this.paymentRepository.create();
+    order.payment = payment;
+    order.status = OrderStatus.AWAITING_SHIPMENT;
+    return this.orderRepository.save(order);
+  }
+}
